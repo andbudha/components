442ae32862f70c3a246434a963f09ca17c817db4
andbudha <andreibartov@gmail.com>
2023-04-13 13:11:48 +0200

    /Dropdown updated / styles added

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -22,7 +22,11 @@ export const DropDown = ({options, value, onChange}) => {
 
     const renderedOptions = options.map(option=>{
         return(
-            <div key={option.value} onClick={()=>handleOptionClick(option)}>
+            <div
+                key={option.value}
+                onClick={()=>handleOptionClick(option)}
+                className={'hover:bg-sky-100 rounded cursor-pointer p-1'}
+            >
                 {option.label}
             </div>
         );
@@ -30,9 +34,12 @@ export const DropDown = ({options, value, onChange}) => {
 
 
     return (
-        <div>
-            <div onClick={handleClick}>{value?.label || 'Select...'}</div>
-            {isOpen && <div>{renderedOptions}</div>}
+        <div className={'w-48 relative m-3'}>
+            <div
+                onClick={handleClick}
+                className={'flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full'}
+            >{value?.label || 'Select...'}</div>
+            {isOpen && <div className={'absolute top-full border rounded p-3 shadow bg-white w-full'}>{renderedOptions}</div>}
         </div>
     );
 };
